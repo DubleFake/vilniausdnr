72b845504d61cb7857576235f8ed599edff92b04
JustasMit <segjustas12@gmail.com>
2022-02-15 15:10:40 +0200

    test responsive tiles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import * as React from "react"
+import PropTypes from "prop-types"
+import Box from "@mui/material/Box"
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+const Item = (props) => {
+	const { sx, ...other } = props
+	return (
+		<Box
+			sx={{
+				bgcolor: (theme) => (theme.palette.mode === "dark" ? "#FFFFFF" : "#000009"),
+				color: (theme) => (theme.palette.mode === "dark" ? "grey.300" : "grey.800"),
+				border: "1px solid",
+				borderColor: (theme) => (theme.palette.mode === "dark" ? "grey.800" : "grey.300"),
+				p: 1,
+				borderRadius: 2,
+				textAlign: "center",
+				fontSize: "0.875rem",
+				fontWeight: "700",
+				...sx,
+			}}
+			{...other}
+		/>
+	)
 }
 
+Item.propTypes = {
+	sx: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+		PropTypes.func,
+		PropTypes.object,
+	]),
+}
+
+const App = () => {
+  const matches = useMediaQuery('(min-width:1000px)');
+	return (
+		<div style={{ width: "100%" }}>
+			<Box
+				sx={{
+					display: "grid",
+					gridAutoFlow: "row",
+					gridTemplateColumns: matches ? "repeat(5, 1fr)" : "repeat(3, 1fr)" ,
+					gridTemplateRows: "repeat(3, 200px)",
+					gap: 1,
+				}}
+			>
+				<Item sx={{ gridColumn: "1 / 3", gridRow: "1 / 3" }}>1</Item>
+				<Item>2</Item>
+				<Item>3</Item>
+				<Item>4</Item>
+				<Item sx={{ gridColumn: matches ? "5" : "3", gridRow: "1 / 3" }}>5</Item>
+			</Box>
+		</div>
+	)
+}
 export default App
