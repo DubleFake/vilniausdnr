d8610398f2e74c0a1c5e5b6a9662f82a046b8ed5
JustasMit <44390650+JustasMit@users.noreply.github.com>
2022-03-08 09:24:21 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -20,13 +20,24 @@ i18n
 	// init i18next
 	// for all options read: https://www.i18next.com/overview/configuration-options
 	.init({
+		// lng: "lt", //default
 		fallbackLng: "lt",
 		debug: true,
 		interpolation: {
 			escapeValue: false, // not needed for react as it escapes by default
 		},
 		detection: {
-			order: ["path", "cookie", "localStorage", "sessionStorage"],
+			order: [
+        "path",
+        "cookie",
+        "localStorage",
+        "sessionStorage",
+				"navigator",
+				"querystring",
+				"htmlTag",
+				"subdomain",
+			],
+
 			lookupFromPathIndex: 0,
 			lookupFromSubdomainIndex: 0,
 		},
