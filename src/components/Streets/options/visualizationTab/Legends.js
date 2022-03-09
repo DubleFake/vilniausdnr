1213ad8ddcd70149c4ea1e30b0981a9115788d92
JustasMit <44390650+JustasMit@users.noreply.github.com>
2022-03-09 14:36:29 +0200

    street legend test

--- src/components/Streets/options/visualizationTab/Legends.js
+++ src/components/Streets/options/visualizationTab/Legends.js
@@ -1,14 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { useTranslation } from "react-i18next"
-
-import { ReactComponent as ObjectCode1 } from "../../../../utils/icons/legendIcons/Atmint_atminimo_lenta.svg"
-import { ReactComponent as ObjectCode2 } from "../../../../utils/icons/legendIcons/Atmint_skulptura.svg"
-import { ReactComponent as ObjectCode3 } from "../../../../utils/icons/legendIcons/Atmint_atminimo_lenta_bareljefas.svg"
-import { ReactComponent as ObjectCode4 } from "../../../../utils/icons/legendIcons/Atmint_freska.svg"
-import { ReactComponent as ObjectCode5 } from "../../../../utils/icons/legendIcons/Atmint_pavadinimo_lentele.svg"
-import { ReactComponent as ObjectCode6 } from "../../../../utils/icons/legendIcons/Atmint_uzrasas.svg"
-import { ReactComponent as ObjectCode7 } from "../../../../utils/icons/legendIcons/Atmint_vietos_pazymejimas.svg"
-import { ReactComponent as ObjectCode8 } from "../../../../utils/icons/legendIcons/Atmint_paminklas.svg"
+import { objectRenderer } from "../../../../utils/streetsArcgisItems"
 
 import Box from "@mui/material/Box"
 import List from "@mui/material/List"
@@ -16,55 +8,12 @@ import ListItem from "@mui/material/ListItem"
 import Divider from "@mui/material/Divider"
 import Typography from "@mui/material/Typography"
 import SvgIcon from "@mui/material/SvgIcon"
-
-const objectIconMap = {
-	Code1: ObjectCode1,
-	Code2: ObjectCode2,
-	Code3: ObjectCode3,
-	Code4: ObjectCode4,
-	Code5: ObjectCode5,
-	Code6: ObjectCode6,
-	Code7: ObjectCode7,
-	Code8: ObjectCode8,
-}
+import ColorLensIcon from "@mui/icons-material/ColorLens"
 
 const Legends = (props) => {
 	const { t, i18n } = useTranslation()
-
-	const objectLegendsList = [
-		{
-			alias: t("plaques.options.objects.plaquePerson"),
-			code: 1,
-		},
-		{
-			alias: t("plaques.options.objects.sculpture"),
-			code: 2,
-		},
-		{
-			alias: t("plaques.options.objects.plaqueOther"),
-			code: 3,
-		},
-		{
-			alias: t("plaques.options.objects.mural"),
-			code: 4,
-		},
-		{
-			alias: t("plaques.options.objects.plaqueTitle"),
-			code: 5,
-		},
-		{
-			alias: t("plaques.options.objects.sign"),
-			code: 6,
-		},
-		{
-			alias: t("plaques.options.objects.marker"),
-			code: 7,
-		},
-		{
-			alias: t("plaques.options.objects.monument"),
-			code: 8,
-		},
-	]
+	const uniqueValues = objectRenderer.uniqueValueInfos
+	console.log(uniqueValues)
 
 	return (
 		<Box sx={{ width: 350, bgcolor: "background.paper" }}>
@@ -72,7 +21,18 @@ const Legends = (props) => {
 				{t("plaques.options.legend") + ":"}
 			</Typography>
 			<List>
-				{objectLegendsList.map((legend) => (
+				{uniqueValues.map((legend, i) => (
+					<div key={i}>
+						<ListItem sx={{ my: 0.3 }} disablePadding>
+							<ColorLensIcon sx={{ ml: 2, mr: 2, fontSize: 35, color: legend.symbol.color }} />
+							<Typography sx={{ mr: 1 }} variant="body2">
+								{legend.value}
+							</Typography>
+						</ListItem>
+						{i !== 13 && <Divider light variant="middle" />}
+					</div>
+				))}
+				{/* {objectLegendsList.map((legend) => (
 					<div key={legend.code}>
 						{props.visibleObjectIcons.length ? (
 							props.visibleObjectIcons.includes(legend.code) && (
@@ -108,7 +68,7 @@ const Legends = (props) => {
 							</>
 						)}
 					</div>
-				))}
+				))} */}
 			</List>
 		</Box>
 	)
--- src/utils/streetsArcgisItems.js
+++ src/utils/streetsArcgisItems.js
@@ -10,10 +10,165 @@ import Locate from "@arcgis/core/widgets/Locate"
 const url = window.location.href
 const origin = new URL(url).origin
 
+const colors = [
+	"#b2b2b2",
+	"#b2b2b2",
+	"#a87000",
+	"#267300",
+	"#4ce600",
+	"#cdf57a",
+	"#e64b41",
+	"#e60000",
+	"#a87000",
+	"#005ce6",
+	"#8400a8",
+	"#4e4e4e",
+	"#c500ff",
+	"#004da8",
+	"#ffff73",
+	"#73b2ff",
+	"#e173ff",
+	"#828282",
+	"#e60000",
+	"#e6e600",
+]
+export const objectRenderer = {
+	type: "unique-value",
+	field: "KATEGOR",
+	uniqueValueInfos: [
+		{
+			value: "D1",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[3],
+			},
+		},
+		{
+			value: "D2",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[4],
+			},
+		},
+		{
+			value: "D",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[5],
+			},
+		},
+		{
+			value: "C2",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[6],
+			},
+		},
+		{
+			value: "D3",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[7],
+			},
+		},
+		{
+			value: "AS",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[8],
+			},
+		},
+		{
+			value: "C1",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[9],
+			},
+		},
+		{
+			value: "B1",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[10],
+			},
+		},
+		{
+			value: "F1",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[12],
+			},
+		},
+		{
+			value: "B2",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[13],
+			},
+		},
+		{
+			value: "P",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[14],
+			},
+		},
+		{
+			value: "E2",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[15],
+			},
+		},
+		{
+			value: "S",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[16],
+			},
+		},
+		{
+			value: "A2",
+			symbol: {
+				type: "simple-line",
+				cap: "butt",
+				color: colors[17],
+			},
+		},
+	],
+	visualVariables: [
+		{
+			type: "size",
+			valueExpression: "$view.scale",
+			stops: [
+				{ size: 14, value: 500 },
+				{ size: 11, value: 1000 },
+				{ size: 8, value: 2000 },
+				{ size: 5, value: 5000 },
+				{ size: 2, value: 10000 },
+			],
+		},
+	],
+}
+
 export const objects = new FeatureLayer({
 	url: "https://utility.arcgis.com/usrsvcs/servers/23aa84d7de1145d0848d2adc03bc4757/rest/services/VilniausDNR/VilniausDNR/MapServer/1",
 	outFields: ["*"],
 	title: "Gatvės",
+	renderer: objectRenderer,
 })
 
 const basemap1 = new Basemap({
