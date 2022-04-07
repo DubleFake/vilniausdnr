861628eea00bd3ae31a3ac579cd4fac9919358e5
JustasMit <44390650+JustasMit@users.noreply.github.com>
2022-04-07 09:53:18 +0300

    add color to number

--- src/components/Signs/options/searchTab/Count.js
+++ src/components/Signs/options/searchTab/Count.js
@@ -8,9 +8,13 @@ const Count = (props) => {
 	const { t, i18n } = useTranslation()
 
 	return (
-		<Box sx={{ ml: 1 }}>
-			<Typography sx={{ float: "left" }} variant="body2">
-				{t("plaques.options.count")}: {props.objectCount}
+		<Box sx={{ ml: 1, float: "left" }}>
+			<Typography variant="body2" component="span">
+				{t("plaques.options.count")}:
+				<Typography sx={{ color: "secondary.main" }} variant="body2" component="span">
+					{" "}
+					{props.objectCount}
+				</Typography>
 			</Typography>
 		</Box>
 	)
