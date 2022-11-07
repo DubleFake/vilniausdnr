174589744275d4e0fd232e654188f535e148dcb8
JustasMit <44390650+JustasMit@users.noreply.github.com>
2022-11-07 13:47:14 +0200

    person names

--- src/components/Persons/personInfo/PersonHeader.js
+++ src/components/Persons/personInfo/PersonHeader.js
@@ -10,15 +10,65 @@ const PersonHeader = (props) => {
 				<Typography
 					sx={{ fontWeight: "bold" }}
 					variant="h3"
-					gutterBottom
+					gutterBottom={props.biographyFeatures[0].attributes.Kuri_kalba_pagrindine === "LT" ? true : false}
 					component="div"
 					align="left"
 					color="#323B4C"
 				>
+					{console.log(props.biographyFeatures)}
 					{props.biographyFeatures[0].attributes.Vardas_lietuviskai +
 						" " +
 						props.biographyFeatures[0].attributes.Pavarde_lietuviskai}
+					{props.biographyFeatures[0].attributes.Pseudonimas_ir_slapyvardziai
+						? "-" + props.biographyFeatures[0].attributes.Pseudonimas_ir_slapyvardziai
+						: null}
 				</Typography>
+
+				{props.biographyFeatures[0].attributes.Kuri_kalba_pagrindine === "EN" ? (
+					<Typography
+						sx={{ fontStyle: "italic" }}
+						color="text.secondary"
+						variant="h5"
+						gutterBottom
+						component="div"
+						align="left"
+					>
+						{props.biographyFeatures[0].attributes.Vardas_pavarde_EN}
+					</Typography>
+				) : props.biographyFeatures[0].attributes.Kuri_kalba_pagrindine === "KITA" ? (
+					<Typography
+						sx={{ fontStyle: "italic" }}
+						color="text.secondary"
+						variant="h5"
+						gutterBottom
+						component="div"
+						align="left"
+					>
+						{props.biographyFeatures[0].attributes.Vardas_pavarde_KITA}
+					</Typography>
+				) : props.biographyFeatures[0].attributes.Kuri_kalba_pagrindine === "PL" ? (
+					<Typography
+						sx={{ fontStyle: "italic" }}
+						color="text.secondary"
+						variant="h5"
+						gutterBottom
+						component="div"
+						align="left"
+					>
+						{props.biographyFeatures[0].attributes.Vardas_pavarde_PL}
+					</Typography>
+				) : props.biographyFeatures[0].attributes.Kuri_kalba_pagrindine === "RU" ? (
+					<Typography
+						sx={{ fontStyle: "italic" }}
+						color="text.secondary"
+						variant="h5"
+						gutterBottom
+						component="div"
+						align="left"
+					>
+						{props.biographyFeatures[0].attributes.Vardas_pavarde_RU}
+					</Typography>
+				) : null}
 
 				<Typography color="text.secondary" variant="body2" gutterBottom component="div" align="left">
 					{props.biographyFeatures[0].attributes.Veikla_kuryba_trumpai}
