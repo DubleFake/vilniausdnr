e4efd3dffa538e0b4b127b40604eef5a8ab7c103
JustasMit <segjustas12@gmail.com>
2022-11-15 12:40:10 +0200

    Update InfoModal.js

--- src/components/Nav/InfoModal.js
+++ src/components/Nav/InfoModal.js
@@ -81,7 +81,7 @@ const InfoModal = () => {
 					</Typography>
 					<iframe
 						width="100%"
-						height="60%"
+						height={window.innerHeight - (window.innerHeight / 10) * 5}
 						src="https://www.youtube.com/embed/Sb59WddVs4I"
 						frameborder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
