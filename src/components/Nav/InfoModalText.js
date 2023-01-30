1d66418f91555925eac547f4e4c3a691b85f3c32
JustasMit <segjustas12@gmail.com>
2023-01-30 13:43:21 +0200

    Update InfoModalText.js

--- src/components/Nav/InfoModalText.js
+++ src/components/Nav/InfoModalText.js
@@ -661,7 +661,7 @@ export const textDict = {
 						},
 					}}
 					variant="contained"
-					onClick={() => window.open("mailto:info@vplanas.lt/?subject=Vilniaus DNR paklausimas", "_self")}
+					onClick={() => window.open("mailto:info@vplanas.lt?subject=Vilniaus DNR paklausimas", "_self")}
 				>
 					Susisiekime
 				</Button>
